b433ff69be53d6cb397a05d8f3312b571da5d4c6
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-09 11:56:47 +0530

    Created a server with the http module.

--- demo_module.js
+++ demo_module.js
@@ -4,4 +4,5 @@ var dt = require('./myfirstmodule');
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('The current date and time: ' + dt.myDateTime());
+    res.end();
 }).listen(8080);
